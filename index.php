84feeb0c8b36a3b009bccb963c6c61c2b2f91184
Ackshon <stephen@ackshon.com>
2016-02-08 10:02:17 -0600

    Added repeater
    Started assembling most common used on index.php.

--- index.php
+++ index.php
@@ -3,6 +3,30 @@
 *	Nothing too out of the ordinary, just wanted my own repo instead
 *	of always using the documentation on the site
 */
-echo "Some ACF Stuff";
 
-?>
+
+// Text Field
+the_field('text');
+
+// Text Area
+the_field('textarea');
+
+// Image Field: Basic display (URL)
+if( get_field('image') ): ?>
+
+	<img src="<?php the_field('image'); ?>" />
+
+<?php endif;
+
+// WYSIWYG
+the_field('wysiwyg_test');
+
+// Repeater
+if( have_rows('repeater_field_name') ):
+	while ( have_rows('repeater_field_name') ) : the_row();
+        // display a sub field value
+        the_sub_field('sub_field_name');
+    endwhile;
+else :
+    // no rows found
+endif;
